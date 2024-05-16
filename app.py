9df429528eb685dcdbe30461db8a82396ef3c5b3
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-17 11:42:02 +1200

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+DATABASE = 'SQL.db'
+
+with sqlite3.connect(DATABASE) as db:
+    cursor = db.cursor()
+    sql = "SELECT * FROM Animals;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+
+for animals in results:
+    #creates list
+    print(f"animal_id: {animals[0]}  animal_name: {animals[1]}  scientific_name:  {animals[2]}  species_types:  {animals[3]}")
+
+db.close()
