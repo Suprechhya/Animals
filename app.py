19ecc2ecd181e1c6b456ce5a60877d57a5328292
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-29 10:23:35 +1200

    added print(f"animal_id      animal_name       scientific_name         species_types")

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import sqlite3
 
 DATABASE = 'SQL.db'
 
-
 #open the database
 db = sqlite3.connect(DATABASE)
 cursor = db.cursor()
@@ -10,13 +9,8 @@ sql = "SELECT * FROM Animals;"
 cursor.execute(sql)
 results = cursor.fetchall()
 
-
 #print all the results
-print(f"animal_id      animal_name      scientific_name         species_types")
+print(f"animal_id      animal_name       scientific_name         species_types")
 for animals in results:
 #creates list
     print(f"animal_id: {animals[0]}  animal_name: {animals[1]}   scientific_name: {animals[2]}    species_types: {animals[3]}")
-
-#close the database
-    
-db.close()
