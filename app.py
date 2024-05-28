bc96b00b4af4a9826525ee8f1f0230ba813ad784
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-29 10:22:54 +1200

    Added print(f"animal_id      animal_name       scientific_name         species_types")

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ results = cursor.fetchall()
 
 
 #print all the results
-print(f"animal_id      animal_name       scientific_name          species_types")
+print(f"animal_id      animal_name      scientific_name         species_types")
 for animals in results:
 #creates list
     print(f"animal_id: {animals[0]}  animal_name: {animals[1]}   scientific_name: {animals[2]}    species_types: {animals[3]}")
