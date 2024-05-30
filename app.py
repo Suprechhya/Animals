e8d50809d5619da091e99257bbefe581bceb2085
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-30 14:53:05 +1200

    Added #open the database def print_all_animals(): db = sqlite3.connect(DATABASE) cursor = db.cursor() sql = "SELECT * FROM Animals;" cursor.execute(sql) results = cursor.fetchall()
    def print_all_animal_id():
    db = sqlite3.connect(DATABASE)
    cursor = db.cursor()
    sql = "SELECT animal_id FROM Animals;"
    cursor.execute(sql)
    results = cursor.fetchall()
    
    def print_all_animal_name():
    db = sqlite3.connect(DATABASE)
    cursor = db.cursor()
    sql = "SELECT animal_name FROM Animals;"
    cursor.execute(sql)
    results = cursor.fetchall()
    
    def print_all_scientific_name():
    db = sqlite3.connect(DATABASE)
    cursor = db.cursor()
    sql = "SELECT scientific_name FROM Animals;"
    cursor.execute(sql)
    results = cursor.fetchall()
    
    def print_all_species_types():
    db = sqlite3.connect(DATABASE)
    cursor = db.cursor()
    sql = "SELECT species_types FROM Animals;"
    cursor.execute(sql)
    results = cursor.fetchall()

--- app.py
+++ app.py
@@ -4,10 +4,38 @@ DATABASE = 'SQL.db'
 
 #open the database
 def print_all_animals():
-    '''print all the animal data nicely'''
 db = sqlite3.connect(DATABASE)
 cursor = db.cursor()
 sql = "SELECT * FROM Animals;"
+cursor.execute(sql)
+results = cursor.fetchall()
+
+def print_all_animal_id():
+db = sqlite3.connect(DATABASE)
+cursor = db.cursor()
+sql = "SELECT animal_id FROM Animals;"
+cursor.execute(sql)
+results = cursor.fetchall()
+
+
+def print_all_animal_name():
+db = sqlite3.connect(DATABASE)
+cursor = db.cursor()
+sql = "SELECT animal_name FROM Animals;"
+cursor.execute(sql)
+results = cursor.fetchall()
+
+def print_all_scientific_name():
+db = sqlite3.connect(DATABASE)
+cursor = db.cursor()
+sql = "SELECT scientific_name FROM Animals;"
+cursor.execute(sql)
+results = cursor.fetchall()
+
+def print_all_species_types():
+db = sqlite3.connect(DATABASE)
+cursor = db.cursor()
+sql = "SELECT species_types FROM Animals;"
 cursor.execute(sql)
 results = cursor.fetchall()
 
@@ -18,7 +46,6 @@ for animals in results:
     print(f"animal_id: {animals[0]}  animal_name: {animals[1]}   scientific_name: {animals[2]}    species_types: {animals[3]}")
 #loop finish here
 db.close()
-
 
 #main code
 user_input = input(
@@ -35,13 +62,15 @@ Which Animal Data Would You Like To See?
 
 if user_input == '1':
     print_all_animals()
-elif user_input =='2':
-    print_all_animal_by_animal_id()
+elif user_input == '2':
+    print_all_animal_id()
 elif user_input =='3':
-    print_all_animal_by_animal_name()
+    print_all_animal_name()
 elif user_input == '4':
-    print_all_animal_by_scientific_name()
+    print_all_scientific_name()
 elif user_input == '5':
-    print_all_animal_by_species_types()
+    print_all_species_types()
+elif user_input == '6':
+    exit
 else:
     print('That is not a option')
