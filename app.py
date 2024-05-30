273b5fb998704f2b6f72d38729d136c3c6c573ff
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-30 15:16:19 +1200

    Added import sqlite3
    DATABASE = "SQL.db"
    
    #funtions
    def print_all_animals():
        '''print all the animals nicely'''
    db = sqlite3.connect('SQL.db')
    cursor= db.cursor()
    sql = "SELECT * FROM Animals"
    cursor.execute(sql)
    results = cursor.fetchall()
    print(results)
    db.close()
    
    #main code
    print_all_animals()

--- app.py
+++ app.py
@@ -1,76 +1,18 @@
 import sqlite3
 
-DATABASE = 'SQL.db'
+DATABASE = "SQL.db"
 
-#open the database
+#funtions
 def print_all_animals():
-db = sqlite3.connect(DATABASE)
-cursor = db.cursor()
-sql = "SELECT * FROM Animals;"
+    '''print all the animals nicely'''
+db = sqlite3.connect('SQL.db')
+cursor= db.cursor()
+sql = "SELECT * FROM Animals"
 cursor.execute(sql)
 results = cursor.fetchall()
-
-def print_all_animal_id():
-db = sqlite3.connect(DATABASE)
-cursor = db.cursor()
-sql = "SELECT animal_id FROM Animals;"
-cursor.execute(sql)
-results = cursor.fetchall()
-
-
-def print_all_animal_name():
-db = sqlite3.connect(DATABASE)
-cursor = db.cursor()
-sql = "SELECT animal_name FROM Animals;"
-cursor.execute(sql)
-results = cursor.fetchall()
-
-def print_all_scientific_name():
-db = sqlite3.connect(DATABASE)
-cursor = db.cursor()
-sql = "SELECT scientific_name FROM Animals;"
-cursor.execute(sql)
-results = cursor.fetchall()
-
-def print_all_species_types():
-db = sqlite3.connect(DATABASE)
-cursor = db.cursor()
-sql = "SELECT species_types FROM Animals;"
-cursor.execute(sql)
-results = cursor.fetchall()
-
-#print all the results
-print(f"animal_id      animal_name       scientific_name         species_types")
-for animals in results:
-#creates list
-    print(f"animal_id: {animals[0]}  animal_name: {animals[1]}   scientific_name: {animals[2]}    species_types: {animals[3]}")
-#loop finish here
+print(results)
 db.close()
 
 #main code
-user_input = input(
-"""
-Which Animal Data Would You Like To See? 
-1. Print all Animal data
-2. Prints the ID of the Animal
-3. Prints the name of the Animal
-4. Prints the scientific name of the Animal
-5. Prints the species types of the Animal
-6. Exit
-"""
-)
+print_all_animals()
 
-if user_input == '1':
-    print_all_animals()
-elif user_input == '2':
-    print_all_animal_id()
-elif user_input =='3':
-    print_all_animal_name()
-elif user_input == '4':
-    print_all_scientific_name()
-elif user_input == '5':
-    print_all_species_types()
-elif user_input == '6':
-    exit
-else:
-    print('That is not a option')
