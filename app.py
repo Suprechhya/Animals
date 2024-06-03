229d8c668f1a0645ac4a3ba17e5056bec5bcd131
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-04 10:44:24 +1200

    Added #loops through all results print('Animal_id     Animal_Name     Scientific_Name    Species_Types') for animals in results:     print(f'Animal_ID   {animals[0]}   Animal_Name:   {animals[1]}    Scientific_Name:  {animals[2]}  Species_Types:  {animals[3]}') #loop finised here db.close()
    #main code
    while True:
        user_input = input('\nWhich Animal Data Would You Like To See? \n1. Print all data \n2. Exit\n')
        if user_input == '1':
            print_all_animals()
        if user_input =='2':
            break
        else:
            print('That is not a option')

--- app.py
+++ app.py
@@ -11,11 +11,25 @@ sql = "SELECT * FROM Animals"
 cursor.execute(sql)
 results = cursor.fetchall()
 #loops through all results
+print('Animal_id     Animal_Name     Scientific_Name    Species_Types')
 for animals in results:
-    print(animals)
+    print(f'Animal_ID   {animals[0]}   Animal_Name:   {animals[1]}    Scientific_Name:  {animals[2]}  Species_Types:  {animals[3]}')
+#loop finised here
 db.close()
-#loop finised
+
+
+
+
+
+
 
 #main code
-print_all_animals()
+while True:
+    user_input = input('\nWhich Animal Data Would You Like To See? \n1. Print all data \n2. Exit\n')
+    if user_input == '1':
+        print_all_animals()
+    if user_input =='2':
+        break
+    else:
+        print('That is not a option')
 
