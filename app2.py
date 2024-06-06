6a5bdce18ec6db0b7b9612a912a480059545f73a
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-07 11:01:35 +1200

    Added #imports data import sqlite3
    #Contants and VariablesS
    DATABASE = 'SQL.db'
    
    #Funtion Code
    def print_all_animals():
        '''print all the animals nicely'''
        db = sqlite3.connect('SQL.db')
        cursor = db.cursor()
        sql = "SELECT * FROM Animals"
        cursor.execute(sql)
        results = cursor.fetchall()
        #loops through all results
        print('Animal_id     Animal_Name     Scientific_Name    Species_Types')
        for animals in results:
            print(f'Animal_ID   {animals[0]}   Animal_Name:   {animals[1]}    Scientific_Name:  {animals[2]}  Species_Types:  {animals[3]}')
        #loop finised here
        db.close()
    
    def print_all_animals_by_id():
        '''print all the animal name nicely'''
        db = sqlite3.connect('SQL.db')
        cursor = db.cursor()
        sql = "SELECT animal_id FROM Animals"
        cursor.execute(sql)
        results = cursor.fetchall()
        for animals in results:
            print(f'Animal_ID:   {animals[0]}')
    
    def print_all_animals_by_name():
        '''print all the animal name nicely'''
        db = sqlite3.connect('SQL.db')
        cursor = db.cursor()
        sql = "SELECT animal_name FROM Animals"
        cursor.execute(sql)
        results = cursor.fetchall()
        for animals in results:
            print(f'Animal_Name:   {animals[1]}')
    
    def print_all_animals_by_scientific_name():
        '''print all the scientific name nicely'''
        db = sqlite3.connect('SQL.db')
        cursor = db.cursor()
        sql = "SELECT scientific_name FROM Animals"
        cursor.execute(sql)
        results = cursor.fetchall()
        for animals in results:
            print(f'Scientific_Name:   {animals[2]}')
    
    def print_all_animals_by_species_types():
        '''print all the species types nicely'''
        db = sqlite3.connect('SQL.db')
        cursor = db.cursor()
        sql = "SELECT species_types FROM Animals"
        cursor.execute(sql)
        results = cursor.fetchall()
        for animals in results:
            print(f'Species_Types:   {animals[3]}')
    
    def print_all_animals_in_alphabetical_order():
        '''print all the animal data nicely'''
        db = sqlite3.connect('SQL.db')
        cursor = db.cursor()
        sql = "SELECT species_types FROM Animals"
        cursor.execute(sql)
        results = cursor.fetchall()
        for animals in results:
                print(f'Animal_ID   {animals[0]}   Animal_Name:   {animals[1]}    Scientific_Name:  {animals[2]}  Species_Types:  {animals[3]}')
    
    #Main Code
    user_input = input(
    """
    Which Data Would You Like To See?
    1. Print All Animal Data
    2. Print Only Animal ID
    3. Print Only Animal Names
    4. Print Only Scientific Names
    5. Print Only Species Types
    6. Print All Animal Data in Alphabetical Order
    7. Exit
    """)
    
    if user_input == '1':
        print_all_animals()
    elif user_input == '2':
        print_all_animals_by_id()
    elif user_input == '3':
        print_all_animals_by_name()
    elif user_input == '4':
        print_all_animals_by_scientific_name()
    elif user_input == '5':
        print_all_animals_by_species_types()
    elif user_input == '6':
        print_all_animals_in_alphabetical_order()
    else:
        print('That was not an option')

--- app2.py
+++ app2.py
@@ -5,80 +5,78 @@ import sqlite3
 #Contants and VariablesS
 DATABASE = 'SQL.db'
 
-
 #Funtion Code
 def print_all_animals():
     '''print all the animals nicely'''
-db = sqlite3.connect('SQL.db')
-cursor= db.cursor()
-sql = "SELECT * FROM Animals"
-cursor.execute(sql)
-results = cursor.fetchall()
-#loops through all results
-print('Animal_id     Animal_Name     Scientific_Name    Species_Types')
-for animals in results:
-    print(f'Animal_ID   {animals[0]}   Animal_Name:   {animals[1]}    Scientific_Name:  {animals[2]}  Species_Types:  {animals[3]}')
-#loop finised here
-db.close()
+    db = sqlite3.connect('SQL.db')
+    cursor = db.cursor()
+    sql = "SELECT * FROM Animals"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loops through all results
+    print('Animal_id     Animal_Name     Scientific_Name    Species_Types')
+    for animals in results:
+        print(f'Animal_ID   {animals[0]}   Animal_Name:   {animals[1]}    Scientific_Name:  {animals[2]}  Species_Types:  {animals[3]}')
+    #loop finised here
+    db.close()
 
 def print_all_animals_by_id():
     '''print all the animal name nicely'''
-db = sqlite3.connect('SQL.db')
-cursor= db.cursor()
-sql = "SELECT animal_id FROM Animals"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(f'Animal_ID:   {animals[0]}')
+    db = sqlite3.connect('SQL.db')
+    cursor = db.cursor()
+    sql = "SELECT animal_id FROM Animals"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for animals in results:
+        print(f'Animal_ID:   {animals[0]}')
 
 
 
 def print_all_animals_by_name():
     '''print all the animal name nicely'''
-db = sqlite3.connect('SQL.db')
-cursor= db.cursor()
-sql = "SELECT animal_name FROM Animals"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(f'Animal_Name:   {animals[1]}')
+    db = sqlite3.connect('SQL.db')
+    cursor = db.cursor()
+    sql = "SELECT animal_name FROM Animals"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for animals in results:
+        print(f'Animal_Name:   {animals[1]}')
 
 
 
-def print_all_animals_by_sciemtific_name():
+def print_all_animals_by_scientific_name():
     '''print all the scientific name nicely'''
-db = sqlite3.connect('SQL.db')
-cursor= db.cursor()
-sql = "SELECT scientific_name FROM Animals"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(f'Scientific_Name:   {animals[2]}')
+    db = sqlite3.connect('SQL.db')
+    cursor = db.cursor()
+    sql = "SELECT scientific_name FROM Animals"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for animals in results:
+        print(f'Scientific_Name:   {animals[2]}')
 
 
 def print_all_animals_by_species_types():
     '''print all the species types nicely'''
-db = sqlite3.connect('SQL.db')
-cursor= db.cursor()
-sql = "SELECT species_types FROM Animals"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(f'Species_Types:   {animals[3]}')
+    db = sqlite3.connect('SQL.db')
+    cursor = db.cursor()
+    sql = "SELECT species_types FROM Animals"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for animals in results:
+        print(f'Species_Types:   {animals[3]}')
 
 
 
 def print_all_animals_in_alphabetical_order():
     '''print all the animal data nicely'''
-db = sqlite3.connect('SQL.db')
-cursor= db.cursor()
-sql = "SELECT * FROM Animals ORDER BY animal_name"
-cursor.execute(sql)
-results = cursor.fetchall()
-print('Animal_id     Animal_Name     Scientific_Name    Species_Types')
-for animals in results:
-    print(f'Animal_ID   {animals[0]}   Animal_Name:   {animals[1]}    Scientific_Name:  {animals[2]}  Species_Types:  {animals[3]}')
-#loop finised here
-db.close()
-
-
-
+    db = sqlite3.connect('SQL.db')
+    cursor = db.cursor()
+    sql = "SELECT species_types FROM Animals"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for animals in results:
+            print(f'Animal_ID   {animals[0]}   Animal_Name:   {animals[1]}    Scientific_Name:  {animals[2]}  Species_Types:  {animals[3]}')
+   
 #Main Code 
 user_input = input(
 """
@@ -100,12 +98,14 @@ elif user_input == '2':
 elif user_input == '3':
     print_all_animals_by_name()
 elif user_input == '4':
-    print_all_animals_by_sciemtific_name()
+    print_all_animals_by_scientific_name()
 elif user_input == '5':
     print_all_animals_by_species_types()
 elif user_input == '6':
     print_all_animals_in_alphabetical_order()
-elif user_input == '7':
-    break
 else:
     print('That was not an option')
+
+
+
+
